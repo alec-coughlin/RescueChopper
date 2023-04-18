936f5dac630ce640baba574275a09813f9c49563
Alec Coughlin <Alec Coughlin>
2023-04-18 15:19:25 -0400

    Face Detection test

--- camera_opencv.py
+++ camera_opencv.py
@@ -1,4 +1,8 @@
+# wget https://github.com/opencv/opencv/raw/master/data/haarcascades/haarcascade_frontalface_default.xml
 import cv2
+
+# Load the Haar Cascade Classifier for face detection
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
 # Load the captured image
 image = cv2.imread('test_image.jpg')
@@ -6,11 +10,22 @@ image = cv2.imread('test_image.jpg')
 if image is None:
     print("Error: Unable to load the image")
 else:
-    # Perform any desired processing here
-    # For example, you can convert the image to grayscale:
-    # gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    # Convert the image to grayscale
+    gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-    # Display the image
+    # Detect faces in the grayscale image
+    faces = face_cascade.detectMultiScale(gray_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+
+    if len(faces) > 0:
+        print("Face detected")
+    else:
+        print("No face detected")
+
+    # Draw rectangles around the detected faces
+    for (x, y, w, h) in faces:
+        cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+
+    # Display the image with detected faces
     cv2.imshow('Captured Image', image)
 
     # Wait for a keypress and close the window
