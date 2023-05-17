b44b7f119c653bdc4abc1537ede9880d43227acd
alec-coughlin <119661906+alec-coughlin@users.noreply.github.com>
2023-05-17 13:47:23 -0400

    Update opencv_camera_test.py

--- opencv_camera_test.py
+++ opencv_camera_test.py
@@ -1,24 +1,9 @@
 import cv2
 
-# Define GStreamer pipeline
-gst_pipeline = (
-    "v4l2src device=/dev/video0 ! "
-    "video/x-raw, width=640, height=480, framerate=30/1, format=UYVY ! "
-    "videoconvert ! "
-    "video/x-raw, format=BGR ! appsink"
-)
-
-# Initialize video capture with GStreamer pipeline
-cap = cv2.VideoCapture(gst_pipeline, cv2.CAP_GSTREAMER)
-
-while True:
+for i in range(16):
+    cap = cv2.VideoCapture(f"v4l2src device=/dev/video{i} ! videoconvert ! appsink", cv2.CAP_GSTREAMER)
     ret, frame = cap.read()
-
-    if not ret:
-        print("Error capturing frame. Check camera connection.")
+    if ret:
+        print(f"Camera found at /dev/video{i}")
         break
-
-    # Show the video
-    cv2.imshow("Camera Test", frame)
-
-    # Exit the loop when 'q'
+    cap.release()
