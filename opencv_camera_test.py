8a0732a1ef16fdd140c11399e96e33530cefe8ad
Alec Coughlin <alec_coughlin@student.uml.edu>
2023-05-17 14:16:36 -0400

    added top output

--- opencv_camera_test.py
+++ opencv_camera_test.py
@@ -1,22 +1,26 @@
+# import the opencv library
 import cv2
 
-# Use video0 as the video source
-cap = cv2.VideoCapture("v4l2src device=/dev/video1 ! videoconvert ! appsink", cv2.CAP_GSTREAMER)
 
-while True:
-    # Capture frame-by-frame
-    ret, frame = cap.read()
+# define a video capture object
+vid = cv2.VideoCapture(0)
 
-    # If frame is read correctly, ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
+while(True):
+	
+	# Capture the video frame
+	# by frame
+	ret, frame = vid.read()
 
-    # Display the resulting frame
-    cv2.imshow('frame', frame)
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+	# Display the resulting frame
+	cv2.imshow('frame', frame)
+	
+	# the 'q' button is set as the
+	# quitting button you may use any
+	# desired button of your choice
+	if cv2.waitKey(1) & 0xFF == ord('q'):
+		break
 
-# When everything done, release the capture
-cap.release()
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
 cv2.destroyAllWindows()
