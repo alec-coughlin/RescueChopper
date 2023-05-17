f7611fdb3bc55839c37b739815feb703ae4a13a0
Alec Coughlin <alec_coughlin@student.uml.edu>
2023-05-17 13:51:50 -0400

    Added errors

--- opencv_camera_test.py
+++ opencv_camera_test.py
@@ -1,6 +1,6 @@
 import cv2
 
-for i in range(16):
+for i in range(32):
     cap = cv2.VideoCapture(f"v4l2src device=/dev/video{i} ! videoconvert ! appsink", cv2.CAP_GSTREAMER)
     ret, frame = cap.read()
     if ret:
