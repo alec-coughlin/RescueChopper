6d8374c61ee8eedae6198bac73237270e2d53d0f
alec-coughlin <119661906+alec-coughlin@users.noreply.github.com>
2023-05-17 13:16:00 -0400

    Update HOG_human_detection_test.py

--- HOG_human_detection_test.py
+++ HOG_human_detection_test.py
@@ -1,32 +1,37 @@
 import cv2
 import imutils
 
-# Initialize the HOG descriptor for human detection
+# load the serialized HOG + Linear SVM model
+# this model was trained on the INRIA person dataset and can detect people in images
 hog = cv2.HOGDescriptor()
 hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-# Initialize video capture
+# open the video stream from the Arducam
 cap = cv2.VideoCapture(0)
 
+# continuously process frames from the video stream
 while True:
-    ret, frame = cap.read()
-
-    # Resize the frame to reduce computation time
-    frame = imutils.resize(frame, width=min(400, frame.shape[1]))
-
-    # Detect humans in the frame
-    (rects, weights) = hog.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.05)
-
-    # Draw bounding boxes around detected humans
-    for (x, y, w, h) in rects:
-        cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-    # Show the video with detections
-    cv2.imshow("Human Detection", frame)
-
-    # Exit the loop when 'q' is pressed
+    # read a frame from the video stream
+    r, frame = cap.read()
+    if r:
+        # resize the frame to reduce processing time
+        frame = imutils.resize(frame, width=min(400, frame.shape[1]))
+        
+        # detect people in the frame
+        # this returns a list of bounding boxes for detected people
+        (boxes, weights) = hog.detectMultiScale(frame, winStride=(4, 4), padding=(8, 8), scale=1.05)
+        
+        # draw the bounding boxes
+        for (x, y, w, h) in boxes:
+            cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 0, 255), 2)
+        
+        # show the output frame
+        cv2.imshow("output", frame)
+    
+    # if the 'q' key is pressed, break from the loop
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-cap.release()
+# clean up
 cv2.destroyAllWindows()
+cap.release()
