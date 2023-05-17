3574480709a0cbcc3a2b05579b2b47f2129d08fe
alec-coughlin <119661906+alec-coughlin@users.noreply.github.com>
2023-05-17 13:56:24 -0400

    Update opencv_camera_test.py

--- opencv_camera_test.py
+++ opencv_camera_test.py
@@ -1,9 +1,22 @@
 import cv2
 
-for i in range(32):
-    cap = cv2.VideoCapture(f"v4l2src device=/dev/video{i} ! videoconvert ! appsink", cv2.CAP_GSTREAMER)
+# Use video0 as the video source
+cap = cv2.VideoCapture("v4l2src device=/dev/video0 ! videoconvert ! appsink", cv2.CAP_GSTREAMER)
+
+while True:
+    # Capture frame-by-frame
     ret, frame = cap.read()
-    if ret:
-        print(f"Camera found at /dev/video{i}")
+
+    # If frame is read correctly, ret is True
+    if not ret:
+        print("Can't receive frame (stream end?). Exiting ...")
         break
-    cap.release()
+
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+# When everything done, release the capture
+cap.release()
+cv2.destroyAllWindows()
