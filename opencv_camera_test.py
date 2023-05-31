314446d24fa8ef72ec708a036f0a1e890053ed8f
Capstone Copter <alec_coughlin@student.uml.edu>
2023-05-31 12:02:34 -0400

    Working with USB on opencv_camera_test.py

--- opencv_camera_test.py
+++ opencv_camera_test.py
@@ -24,6 +24,10 @@ model = YOLO('yolov8n.pt')
 #success = model.export(format='onnx')
 
 cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+# cap.set(cv2.CAP_PROP_FPS, 10)
+
 while cap.isOpened():
     ret, frame = cap.read()
     
