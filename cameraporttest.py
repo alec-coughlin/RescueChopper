0ba60298a659b433ad047cb1da1fe666c7c00a88
alec-coughlin <119661906+alec-coughlin@users.noreply.github.com>
2023-06-14 13:12:34 -0400

    Update cameraporttest.py

--- cameraporttest.py
+++ cameraporttest.py
@@ -1,21 +1,25 @@
+from picamera2 import PiCamera2
+from ultralytics import YOLO
 import cv2
+import numpy as np
 
-def find_camera_index():
-    camera_index = -1
-    max_camera_devices = 5  # You can adjust this number based on your needs
+model = YOLO("yolov8n.onnx")
 
-    for i in range(max_camera_devices):
-        cap = cv2.VideoCapture(i)
-        if cap.isOpened():
-            camera_index = i
-            cap.release()
+with PiCamera2(resolution=(480, 480), framerate=30) as camera:
+    output = np.empty((480, 480, 3), dtype=np.uint8)
+    while True:
+        camera.capture(output, 'bgr')
+        frame = cv2.cvtColor(output, cv2.COLOR_BGR2RGB)
+
+        results = model(frame)
+
+        # Uncomment the following lines if you want to process the results
+        #for r in results:
+        #   boxes = r.boxes  # Boxes object for bbox outputs
+        #   probs = r.probs  # Class probabilities for classification outputs
+        #   print(boxes.xyxy[0])  # print img1 predictions (pixels)
+
+        #cv2.imshow("frame", frame)
+
+        if (cv2.waitKey(30) & 0xFF == 27):  # break with escape key
             break
-
-    return camera_index
-
-camera_index = find_camera_index()
-
-if camera_index == -1:
-    print("Error: No camera found")
-else:
-    print("Camera found at index:", camera_index)
